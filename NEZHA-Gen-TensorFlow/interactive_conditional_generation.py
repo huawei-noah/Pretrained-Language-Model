a8a705e9c8c952e078b45d1091d3f0ed161483d8
Gaia <gaia@GaiadeMacBook-Pro.local>
2021-01-06 10:45:29 +0800

    update

--- NEZHA-Gen-TensorFlow/interactive_conditional_generation.py
+++ NEZHA-Gen-TensorFlow/interactive_conditional_generation.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 # coding=utf-8
-# Changed for Nezha-Gen-Tensorflow
-# Copyright 2018 The Google AI Language Team Authors.
 # 2020/01/01 Changed for Nezha-Gen-Tensorflow. 
 #   Huawei Technologies Co., Ltd. 
 # Copyright (C) 2019. Huawei Technologies Co., Ltd. All rights reserved.
+# Copyright 2018 The Google AI Language Team Authors.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at

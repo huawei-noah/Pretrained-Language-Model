bb167f011618617073b4152063a1c6a46cd16478
Victor <wjqjsnj@gmail.com>
2021-11-02 10:34:33 +0800

    Add files via upload

--- BBPE/bbpe/map_freq.py
+++ BBPE/bbpe/map_freq.py
@@ -1,21 +1,6 @@
-# coding=utf-8
-# Copyright 2020 Huawei Technologies Co., Ltd.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+# -*- coding: UTF-8 -*-
 import re
 import sys
-import six
 import unicodedata
 import collections
 import base64
@@ -111,5 +96,50 @@ for line in sys.stdin:
   #              vocabulary["##"+tokens[0]] = 1
                 
     output.write(" | "+tokens[1])
+
+#    if '##'+tokens[0] not in vocabulary and tokens[0] not in vocabulary:
+#        vocab.write("##"+tokens[0])
+#        vocabulary["##"+tokens[0]] = 1
+#        print("##"+tokens[0])
+#        vocab.write("\n")
+#        vocab.write(tokens[0])
+#        vocabulary[tokens[0]] = 1
+#        print(tokens[0])
+#        vocab.write("\n")
+     
+    
+#    if '##'+tokens[0] not in vocabulary and len(tokens[0]) == 2:
+#        vocab.write("##"+tokens[0])
+#        vocabulary["##"+tokens[0]] = 1
+#        print("##"+tokens[0])
+#        vocab.write("\n")
+#    if '##'+tokens[0] not in vocabulary:
+#        vocab.write("##"+tokens[0])
+#        vocabulary["##"+tokens[0]] = 1
+#        print("##"+tokens[0])
+#        vocab.write("\n")
+        
+#    if tokens[0] not in vocabulary:
+#        vocab.write(tokens[0])
+#        vocabulary[tokens[0]] = 1
+#        print(tokens[0])
+#        vocab.write("\n")
+
+#    if '##'+tokens[0] not in vocabulary and len(tokens[0]) > 2:
+#        vocab.write(tokens[0])
+#        vocabulary[tokens[0]] = 1
+#        vocab.write("\n")
+#
+#    if '##'+tokens[0] not in vocabulary and len(tokens[0]) == 2:
+#        vocab.write("##"+tokens[0])
+#        vocabulary["##"+tokens[0]] = 1
+#        print("##"+tokens[0])
+#        vocab.write("\n")
+#        
+#    if tokens[0] not in vocabulary and len(tokens[0]) == 2:
+#        vocab.write(tokens[0])
+#        vocabulary[tokens[0]] = 1
+#        print(tokens[0])
+#        vocab.write("\n")
 
     output.write("\n")
